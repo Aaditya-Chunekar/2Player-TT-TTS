75d82adbfe425be0d363b86f28c1e79debd95513
AadityaAmitChunekar <146899562+Aaditya-Chunekar@users.noreply.github.com>
2024-05-02 17:58:18 +0530

    Update tt.py
    Added pyautogui library's hotkey function to shift to command prompt screen again

--- tt.py
+++ tt.py
@@ -9,12 +9,12 @@ def ps(text):
     myobj = gt(text=text, lang=language, slow=False)
     myobj.save("temp.mp3")
     os.system("start temp.mp3")
-   
 po=input("Enter player one name : ")
 pt=input("Enter player two name : ")
 #deuce-less two player one match system
 ps("Toss for serve")
 time.sleep(3)
+pag.hotkey('alt','tab')
 ch=int(input(str(po)+" selects choice, either 0 or 1, winner serves	"))
 if (random.randint(0,1)==ch):
 	se=po
@@ -24,12 +24,14 @@ else:
 	nse=po
 ps("Enter 1 to increment point of "+str(po)+" and 2 to increment point of "+str(pt)+" after match begins")
 time.sleep(5)
+pag.hotkey('alt','tab')
 ps("Match begins in 3")
 time.sleep(1.5)
 ps("2")
 time.sleep(1.5)
 ps("1")
 time.sleep(1.5)
+pag.hotkey('alt','tab')
 spo=0
 spt=0
 #add deuce logic by 
@@ -38,9 +40,11 @@ while (spo != 11 and spt != 11):
 	if((spo+spt)%4==0 or (spo+spt)%4==1):
 		ps(str(se)+" serves")
 		time.sleep(1.5)
+		pag.hotkey('alt','tab')
 	else:
 		ps(str(nse)+" serves")
 		time.sleep(1.5)
+		pag.hotkey('alt','tab')
 	ptm=int(input("Enter 1 or 2 : "))
 	if(ptm==1):
 		spo+=1;
@@ -48,9 +52,8 @@ while (spo != 11 and spt != 11):
 		spt+=1;
 	ps("Score\n"+str(po)+" "+str(spo)+"\n"+str(pt)+str(spt))
 	time.sleep(5)
+	pag.hotkey('alt','tab')
 if(spo==11):
 	ps(str(po)+" Wins !")
 else:
 	ps(str(pt)+" Wins !")
-
-
